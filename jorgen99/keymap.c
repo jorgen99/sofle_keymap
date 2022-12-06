01ada751b7670b807ec5dd7ede5c0ed865a67d5e
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2022-12-06 21:22:11 +0100

    Bsp on left thumb, Esc on right.
    Also, hold spc for Rshift.

--- jorgen99/keymap.c
+++ jorgen99/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   -  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            |  L3  | LAlt |     | L1   | / L3    /       \ L2   \  | Spc  |  L4  | TG5  | RCSG |
- *            |      |      |      | Enter|/ Esc   /         \ Bsp  \ |      |      |      |      |
+ *            |      |      |      | Enter|/ Bsp   /         \ Esc  \ | RShft|      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
 [_QWERTY] = LAYOUT(
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,    KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,      KC_LBRC,
   KC_LCTL,   KC_A,    KC_S,   KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,   KC_QUOT,
   KC_LSFT,   KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,  KC_MUTE,  KC_MPLY, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,   KC_RSFT,
-    MO(3),  KC_LALT, KC_LGUI, LT(1,KC_ENT),  LT(3,KC_ESC), LT(2,KC_BSPC), KC_SPC, MO(4), TG(5), OSM(MOD_RCTL|MOD_RSFT|MOD_RGUI)
+        MO(3),  KC_LALT, KC_LGUI, LT(1,KC_ENT),  LT(3,KC_BSPC), LT(2,KC_ESC), RSFT_T(KC_SPC), MO(4), TG(5), OSM(MOD_RCTL|MOD_RSFT|MOD_RGUI)
 ),
 
 /*
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      |      |      |      | /       /       \ Bsp  \  |      |      |      |      |
+ *            |      |      |      |      | / Bsp   /       \      \  |      |      |      |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO,     LSFT(KC_1), LSFT(KC_2),   LSFT(KC_3), LSFT(KC_4),    LSFT(KC_5),                 LSFT(KC_6),    LSFT(KC_7),    LSFT(KC_8), LSFT(KC_9), LSFT(KC_0),  LSFT(KC_MINS),
   KC_NO,     KC_1,       KC_2,         KC_3,       KC_4,          KC_5,      KC_6, KC_7,      KC_8,          KC_9,          KC_0,       KC_NO,      KC_NO,       KC_NO,
   KC_NO,     KC_NO,      KC_NO,        KC_NO,      KC_NO,         KC_NO,                      KC_NO,         KC_NO,         KC_NO,      KC_NO,      KC_NO,       KC_NO,
-    KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO,                                                        KC_BSPC, KC_TRNS, KC_NO, KC_NO, KC_NO
+  KC_NO, KC_NO, KC_NO, KC_TRNS, KC_BSPC,                                                      KC_NO,   KC_TRNS, KC_NO, KC_NO, KC_NO
 ),
 
 /*
