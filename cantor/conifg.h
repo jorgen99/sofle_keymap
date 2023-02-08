5fe7f3ecda6f48b1b22cfc90ea88e564c07e7fe0
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2023-02-08 09:47:10 +0100

    Cantor: experimenting with tap-times

---
+++ cantor/conifg.h
@@ -0,0 +1,23 @@
+#pragma once
+
+/*
+  This makes tap and hold keys (like Layer Tap) work better for fast
+  typists, or for high TAPPING_TERM settings.
+
+  If you press a dual-role key, tap another key (press and release)
+  and then release the dual-role key, all within the tapping term, by
+  default the dual-role key will perform its tap action. If the
+  PERMISSIVE_HOLD option is enabled, the dual-role key will perform
+  its hold action instead.
+
+  https://docs.qmk.fm/#/tap_hold?id=tap-or-hold-decision-modes
+*/
+#define PERMISSIVE_HOLD
+
+#ifdef TAPPING_TERM
+    #undef TAPPING_TERM
+    #define TAPPING_TERM 200
+#endif
+
+#define TAPPING_TERM_PER_KEY
+
--- cantor/keymap.c
+++ cantor/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |   Ö  |  Ä   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------.    .-------|   N  |   M  |   ,  |   .  |   -  |RShift|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------.    .-------|   N  |   M  |   ,  |   .  |   -  | Alt  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                          |     | L1   | / L3    /       \ L2   \  | RShft|  L4  |
  *                          |      | Enter|/ Bsp   /         \ Esc  \ | Spc  |  Tab |
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_split_3x6_3(
   KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,            KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC,
   KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,            KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,            KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+  KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,            KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LALT,
                      KC_LGUI, L1_ENT, L3_BSP,    L2_ESC, SFT_SPC, L4_TAB
 ),
 
@@ -111,9 +111,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * L2
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-1 * |      |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
+ * |      |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |   !  |   "  |   #  |   €  |   %  |-------|    |-------|   &  |   /  |   (  |   )  |   =  |   ?  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -122,9 +122,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                          `--------------------'           '------''---------------
  */
 [_NUMBERS] = LAYOUT_split_3x6_3(
-  _______, _______, _______, _______, _______, _______,                 _______, _______, _______, _______, _______, _______,
-  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-  _______, LSFT1,   LSFT2,   LSFT3,   LSFT4,   LSFT5,                   LSFT6,   LSFT7,   LSFT8,   LSFT9,   LSFT0,   _______,
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11,
+  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_F12,
+  _______, LSFT1,   LSFT2,   LSFT3,   LSFT4,   LSFT5,                   LSFT6,   LSFT7,   LSFT8,   LSFT9,   LSFT0,  _______,
                                   _______, _______, KC_BSPC,       _______, _______, _______
 ),
 
@@ -164,7 +164,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_MULTIMEDIA] = LAYOUT_split_3x6_3(
   _______, KC_ACL0, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U,               _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______,
   _______, KC_ACL2, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,               _______, KC_BRMD, _______, KC_BRMU, _______, _______,
-  _______, _______, _______, _______, _______, _______,               KC_MRWD, KC_MPLY, KC_MFFD, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,               _______, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______,
                                   _______, _______, _______,       _______, _______, _______
 ),
 
@@ -203,3 +203,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   └───┤Bsp│   │Ent├───┘
  *                       └───┘   └───┘
  */
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LT(1,KC_ENT):
+            return TAPPING_TERM - 70;
+        default:
+            return TAPPING_TERM;
+    }
+}
+
