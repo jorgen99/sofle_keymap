c9a1d040777a008f48b6b28181517df8f51f486b
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2023-07-19 13:00:18 +0200

    Tapping terms for LT1 and SFT_SPC

--- splitkbcorne/conifg.h
+++ splitkbcorne/conifg.h
@@ -4,7 +4,8 @@
   https://docs.qmk.fm/#/tap_hold?id=tap-or-hold-decision-modes
 */
 // #define PERMISSIVE_HOLD
-#define HOLD_ON_OTHER_KEY_PRESS
+//#define HOLD_ON_OTHER_KEY_PRESS
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 
 #ifdef TAPPING_TERM
     #undef TAPPING_TERM
